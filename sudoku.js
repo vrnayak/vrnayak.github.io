edccfe3e724338057890560dbbc29d1a7bd4643c
vrnayak <vrnayak@umich.edu>
2020-06-25 12:20:48 -0400

    Integrated jQuery functionality into existing JS

--- sudoku.js
+++ sudoku.js
@@ -39,35 +39,47 @@ function createGrid(gridID) {
             if (demoGrid[cellNum] !== 0)
                 newCell.innerHTML = demoGrid[cellNum];
             if (row === 2 || row === 5)
-                newCell.style.borderBottomWidth = "0.3em";
+                $(newCell).css("border-bottom-width", "0.3em");
             if (col === 2 || col === 5)
-                newCell.style.borderRightWidth = "0.3em";
+                $(newCell).css("border-right-width", "0.3em");
             if (demoGrid[cellNum] === 0)
-                newCell.classList.add("userFill");
+                $(newCell).addClass("userFill");
 
-            newCell.id = getID(cellNum);
+            newCell.id = getID(cellNum).slice(1);
         }
     }
 }
 
 function colorDemoGrid(colors) {
 
-    let rows = document.getElementsByTagName("tr");
-    for (let i = 0; i < rows.length; i++) {
-        rows[i].onmouseover = function() { rows[i].classList.add(colors["row"]); };
-        rows[i].onmouseout = function() { rows[i].classList.remove(colors["row"]); };
-    }
+    $("tr").on("mouseover", function() { $(this).addClass(colors["row"]); })
+            .on("mouseout", function() { $(this).removeClass(colors["row"]); });
 
-    let cells = document.getElementsByTagName("td");
-    for (let j = 0; j < cells.length; j++) {
-        cells[j].onmouseover = function() {
+    $("td").on("mouseover", function () {
 
-            // Set colors for column cells
-            let cellNum = cells[j].id.slice(4, 6)
+        // Set colors for column cells
+        let cellNum = $(this).attr("id").slice(4, 6);
+        let col = parseInt(cellNum) % 9;
+        while (col < 81) {
+            $(getID(col)).addClass(colors["col"]);
+            col += 9;
+        }
+
+        // Set colors for box cells
+        let box = getBox(cellNum);
+        let boxStart = Math.floor(box / 3) * 27 + (box % 3) * 3;
+        for (let pos = boxStart; pos < boxStart + 27; pos += 9) {
+            for (let offset = 0; offset < 3; offset++) {
+                $(getID(pos + offset)).addClass(colors["box"]);
+            }
+        }
+    })
+            .on("mouseout", function () {
+
+            let cellNum = $(this).attr("id").slice(4, 6);
             let col = parseInt(cellNum) % 9;
             while (col < 81) {
-                let sameColCell = document.getElementById(getID(col));
-                sameColCell.classList.add(colors["col"]);
+                $(getID(col)).removeClass(colors["col"]);
                 col += 9;
             }
 
@@ -76,34 +88,10 @@ function colorDemoGrid(colors) {
             let boxStart = Math.floor(box / 3) * 27 + (box % 3) * 3;
             for (let pos = boxStart; pos < boxStart + 27; pos += 9) {
                 for (let offset = 0; offset < 3; offset++) {
-                    let final = pos + offset;
-                    let finalCell = document.getElementById(getID(final));
-                    finalCell.classList.add(colors["box"]);
+                    $(getID(pos + offset)).removeClass(colors["box"]);
                 }
             }
-        }
-        cells[j].onmouseout = function() {
-
-            let cellNum = cells[j].id.slice(4, 6);
-            let col = parseInt(cellNum) % 9;
-            while (col < 81) {
-                let sameColCell = document.getElementById(getID(col));
-                sameColCell.classList.remove(colors["col"]);
-                col += 9;
-            }
-
-            // Set colors for box cells
-            let box = getBox(cellNum);
-            let boxStart = Math.floor(box / 3) * 27 + (box % 3) * 3;
-            for (let pos = boxStart; pos < boxStart + 27; pos += 9) {
-                for (let offset = 0; offset < 3; offset++) {
-                    let final = pos + offset;
-                    let finalCell = document.getElementById(getID(final));
-                    finalCell.classList.remove(colors["box"]);
-                }
-            }
-        }
-    }
+        })
 }
 
 // Helper Functions
@@ -114,7 +102,7 @@ function getBox(cellNum) {
     return 3 * rowShift + colShift;
 }
 
-function getID(cellNum) { return "cell" + cellNum.toString(); }
+function getID(cellNum) { return "#cell" + cellNum.toString(); }
 
 // **** SUDOKU.HTML ****
 
@@ -123,20 +111,20 @@ let acceptable = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "Backspace"];
 
 function setEventListeners() {
 
-    document.body.onkeydown = function (e) {
+    $("body").on("keydown", function (e) {
 
-        let selected = document.getElementsByClassName("selectedCell");
+        let selected = $(".selectedCell");
         if (selected.length !== 0) {
 
-            if (selected[0].classList.contains("userFill") === true) {
-                if (acceptable.includes(e.key)) {
-                    if (e.key === "Backspace") selected[0].innerText = "";
-                    else selected[0].innerText = e.key;
-
-                } else selected[0].innerText = "";
+            let cell = selected[0];
+            if ($(cell).hasClass("userFill")) {
+                if (acceptable.includes(e.key) && e.key !== "Backspace")
+                    cell.innerText = e.key;
+                else
+                    cell.innerText = "";
             }
         }
-    }
+    });
 }
 
 function loadTimer() {
@@ -144,7 +132,6 @@ function loadTimer() {
     setTimeout(function () {
         let time = new Date();
         let elapsed = time - startTime;
-        let timer = document.getElementById("timer");
 
         let minutes = Math.floor(elapsed / 60000);
         let seconds = Math.floor((elapsed - (60000 * minutes)) / 1000);
@@ -152,49 +139,36 @@ function loadTimer() {
         minutes = (minutes < 10) ? "0" + minutes.toString() : minutes.toString();
         seconds = (seconds < 10) ? "0" + seconds.toString() : seconds.toString();
 
-        timer.innerText = minutes + ':' + seconds;
+        $("#timer").text(minutes + ':' + seconds);
         loadTimer();
     }, 1000);
 }
 
 function colorGrid(colors) {
 
-    let rows = document.getElementById("grid").getElementsByTagName("tr");
-    for (let i = 0; i < rows.length; i++) {
-        rows[i].onclick = function() {
+    $("#grid tr").on("click", function () {
+        $("tr").removeClass(colors["row"] + " " + colors["col"])
+        $(this).addClass(colors["row"]);
+    });
+    $("#grid td").on("click", function () {
 
-            let resetRows = document.getElementsByTagName("tr");
-            for (let j = 0; j < resetRows.length; ++j)
-                resetRows[j].classList.remove(colors["row"], colors["col"]);
-            rows[i].classList.add(colors["row"]);
-        };
-    }
+        // Reset all background colors
+        $("td").removeClass(colors["row"] + " " + colors["col"]);
 
-    let cells = document.getElementById("grid").getElementsByTagName("td");
-    for (let j = 0; j < cells.length; j++) {
-        cells[j].onclick = function() {
-
-            // Reset all background colors
-            let gridCells = document.getElementsByTagName("td");
-            for (let i = 0; i < 81; i++)
-                gridCells[i].classList.remove(colors["row"], colors["col"]);
-
-            // Set colors for column cells
-            let col = parseInt(cells[j].id.slice(4, 6)) % 9;
-            while (col < 81) {
-                let sameColCell = document.getElementById(getID(col));
-                sameColCell.classList.add(colors["col"]);
-                col += 9;
-            }
-
-            // Add selectedCell class to current cell
-            if (cells[j].getAttribute("color") !== "#7a7a7a") {
-                let selected = document.getElementsByClassName("selectedCell");
-                if (selected.length > 0) selected[0].classList.remove("selectedCell");
-                cells[j].classList.add("selectedCell");
-            }
+        // Set colors for column cells
+        let col = parseInt($(this).attr("id").slice(4, 6)) % 9;
+        while (col < 81) {
+            $(getID(col)).addClass(colors["col"]);
+            col += 9;
         }
-    }
+
+        // Add selectedCell class to current cell
+        if ($(this).css("color") !== "#7a7a7a") {
+            $(".selectedCell").removeClass("selectedCell");
+            $(this).addClass("selectedCell");
+        }
+    });
+
 }
 
 function createButtons() {
@@ -205,19 +179,17 @@ function createButtons() {
         for (let col = 0; col < 3; col++) {
             let newCell = newRow.insertCell(col);
             newCell.innerHTML = 3 * row + col + 1;
-            newCell.addEventListener("click", function () {
-                fillCell(3 * row + col + 1);
-            })
-
+            $(newCell).on("click", function() { fillCell(3 * row + col + 1); });
         }
     }
 }
 
 function fillCell(num) {
 
-    let selected = document.getElementsByClassName("selectedCell");
+    let selected = $(".selectedCell");
     if (selected.length !== 0) {
-        if (selected[0].classList.contains("userFill") === true)
-            selected[0].innerText = num.toString();
+        let cell = selected[0];
+        if ($(cell).hasClass("userFill"))
+            cell.innerText = num.toString();
     }
 }
